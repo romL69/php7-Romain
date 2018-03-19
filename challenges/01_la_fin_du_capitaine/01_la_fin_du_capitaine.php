4b818166a600f633214261301de4dc94fa01ef25
romainLoire <romain.loire@ynov.com>
2018-03-19 14:41:17 +0100

    d

--- challenges/01_la_fin_du_capitaine/01_la_fin_du_capitaine.php
+++ challenges/01_la_fin_du_capitaine/01_la_fin_du_capitaine.php
@@ -8,8 +8,7 @@ $captain = [
     'famous_last_words' => $captainFamousLastWords = 'AAaaargh...',
     ];
 
-if($captain['is_dead'] === true)
-{
+if($captain['is_dead'] === true) {
     print 'Le capitaine '.$captain['name'];
     print " est mort à l'age de ".$captain['age']." ans.\n";
     print 'Ses derniers mots étaient "'.$captain['famous_last_words'].'"'."\n";
